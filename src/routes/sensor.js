78b73c658781f733bd6fccc44f6ec640820a8366
LuuisPortugal <luisenriquegomesportugal@gmail.com>
2019-09-13 01:38:38 -0300

    setando o socket io para os devices e sensores

--- src/routes/sensor.js
+++ src/routes/sensor.js
@@ -5,7 +5,5 @@ const CONTROLLER_PATH = "../controller/" + basename(__filename);
 
 router.post("/sensors", require(CONTROLLER_PATH).postSensor);
 router.get("/sensors/:mac*?", require(CONTROLLER_PATH).getSensor);
-router.put("/sensors/:mac", require(CONTROLLER_PATH).updateSensor);
-router.delete("/sensors/:mac", require(CONTROLLER_PATH).deleteSensor);
 
 export default router;
