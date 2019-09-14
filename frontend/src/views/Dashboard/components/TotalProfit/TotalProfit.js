c6140d38a3eb76ccfb4da1b0d03234cec71c39fd
jhersonharyson <jherson_k-f@hotmail.com>
2019-09-14 07:00:15 -0300

    put data time in metter biggest

--- frontend/src/views/Dashboard/components/TotalProfit/TotalProfit.js
+++ frontend/src/views/Dashboard/components/TotalProfit/TotalProfit.js
@@ -31,7 +31,7 @@ const useStyles = makeStyles(theme => ({
     width: 32
   },
   co2: {
-    marginTop: 10
+    marginTop: 5
   }
 }));
 
@@ -40,57 +40,52 @@ const TotalProfit = props => {
   const classes = useStyles();
 
   const [maxSensor, setMaxSensor] = React.useState(0);
-  React
-    .useEffect(() => {
-      async function getDevices() {
-        const headers = { authentication: await localStorage.getItem('authentication') };
-        const response = await axios.get('sensors', { headers });
+  const [dataSensor, setDataSensor] = React.useState(null);
+  React.useEffect(() => {
+    async function getDevices() {
+      const headers = {
+        authentication: await localStorage.getItem('authentication')
+      };
+      const response = await axios.get('sensors', { headers });
 
-        if (response.data[0]) {
-          let map = response.data.map(sensor => sensor.value);
-          let max = Math.max(...map);
+      if (response.data[0]) {
+        console.log(response.data[0]);
+        let map = response.data.map((sensor, index) => ({
+          value: sensor.value,
+          index
+        }));
 
-          setMaxSensor(max);
-        }
+        let max = map.sort((a, b) => a.value - b.value)[map.length - 1];
+
+        setDataSensor(max.index);
+        setMaxSensor(max.value);
       }
+    }
 
-      getDevices();
-      Socket.on('postSensor', () => getDevices());
-    }, []);
-
+    getDevices();
+    Socket.on('postSensor', () => getDevices());
+  }, []);
 
   return (
-    <Card
-      {...rest}
-      className={clsx(classes.root, className)}
-    >
+    <Card {...rest} className={clsx(classes.root, className)}>
       <CardContent>
-        <Grid
-          container
-          justify="space-between"
-        >
+        <Grid container justify="space-between">
           <Grid item>
             <Typography
               className={classes.title}
               color="inherit"
               gutterBottom
-              variant="body2"
-            >
+              variant="body2">
               MAIOR MEDIÇÃO
             </Typography>
-            <Typography
-              color="inherit"
-              variant="h3"
-            >
+            <Typography color="inherit" variant="h3">
               {maxSensor} ppm
             </Typography>
-
-            <Typography
-              className={classes.co2}
-              color="inherit"
-              variant="h2"
-            >
+            <Typography className={classes.co2} color="inherit" variant="h2">
               CO²
+            </Typography>
+            <Typography variant="h6" style={{ color: '#fff', fontSize: 11 }}>
+              {dataSensor && new Date(dataSensor).toLocaleString()}
             </Typography>
           </Grid>
           <Grid item>
