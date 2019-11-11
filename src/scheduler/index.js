84a262ad75c243e437c6b0961277f95ea7bbebaf
Alessandro Vinicius Siquiera Santos <alessandro.vncs@gmail.com>
2019-11-11 02:17:20 -0300

    Ativação do scheduler

--- src/scheduler/index.js
+++ src/scheduler/index.js
@@ -2,8 +2,9 @@ import path from 'path';
 import { readdirSync } from 'fs';
 
 export default function (app) {
+
 	 readdirSync(path.join(__dirname))
-		.filter( fileName => (fileName !== 'index.js'))
+		.filter( fileName => (fileName !== 'index.js' && !fileName.endsWith('.map')))
 		.forEach(fileName => {
 			require(path.join(__dirname, path.basename(fileName))).default;
 		}); 
